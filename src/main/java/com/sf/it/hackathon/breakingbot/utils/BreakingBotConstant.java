dfc7af5c41ad42a64a1dbce03057fb7605fa8d95
Avinash Kumar <avinashkumar@avkumar-ltm.internal.salesforce.com>
2017-10-06 00:37:49 +0530

    adding constant file

---
+++ src/main/java/com/sf/it/hackathon/breakingbot/utils/BreakingBotConstant.java
@@ -0,0 +1,5 @@
+package com.sf.it.hackathon.breakingbot.utils;
+
+public interface BreakingBotConstant {
+	public final String DEFAULT_RESULT ="Not Able to process your result as I can't undertand you";
+}
